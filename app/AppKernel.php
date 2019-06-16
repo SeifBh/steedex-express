449bd96fa4974ec2c3d6f2de36e424e732970813
BelHadjAli SeifEddine <belhadjali.seif@gmail.com>
2019-06-16 20:07:04 +0100

    admin restriction

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,9 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
-
-
-
+            new AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {

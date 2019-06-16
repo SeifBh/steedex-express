3df1a3e489421344caa4d32a7c08f09deded935e
BelHadjAli SeifEddine <belhadjali.seif@gmail.com>
2019-06-16 12:28:15 +0100

    Import fos

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function testAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/test.html.twig', []);
+    }
+
 }
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,7 +1,63 @@
 <?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
 /**
- * Created by PhpStorm.
- * User: Seif
- * Date: 16/06/2019
- * Time: 11:13
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
  */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom ;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom ;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="addr", type="string", length=255)
+     */
+    private $addr ;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="addr", type="string", length=255)
+     */
+    private $tel ;
+
+
+
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}

3666906cfcfbfa198097d766886357558d306876
BelHadjAli SeifEddine <belhadjali.seif@gmail.com>
2019-06-16 15:56:41 +0100

    6z

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 
 
 

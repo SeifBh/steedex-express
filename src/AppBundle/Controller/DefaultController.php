6aa6aa5c9d171fc903dd28118e38b65f96644562
SEIF BH HEROKU <belhadjali.seif@gmail.com>
2019-07-06 08:31:43 +0100

    Front End Template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,9 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
 
-    public function indexAction(){
-        return new Response('sdsd');
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
+
+
     public function nbUsersAction()
     {
         $em=$this->getDoctrine()->getManager();

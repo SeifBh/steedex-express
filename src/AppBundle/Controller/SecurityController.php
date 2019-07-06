d1c0d16d74dec97fe6e0ec78418ebad1d92867f0
SEIF BH HEROKU <belhadjali.seif@gmail.com>
2019-07-06 22:06:35 +0100

    upload android app

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityController extends Controller
+{
+
+    /**
+     * @Route("/login", name="login")
+     * @return Response
+     */
+    public function login()
+    {
+        try {
+
+            // Your logic here...
+
+            return new JsonResponse([
+                'success' => "Welcome"
+            ]);
+
+        } catch (\Exception $exception) {
+
+            return new JsonResponse([
+                'success' => false,
+                'code'    => $exception->getCode(),
+                'message' => $exception->getMessage(),
+            ]);
+
+        }
+    }
+
+
+
+}

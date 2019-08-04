4a49bc5c761302b36f0e0692b44ac0e2a8bf2048
SEIF BH HEROKU <belhadjali.seif@gmail.com>
2019-08-04 04:51:07 +0100

    prod 0 version

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -26,4 +26,6 @@ class DefaultController extends Controller
     }
 
 
+
+
 }
--- src/DemandeBundle/Controller/ApiController.php
+++ src/DemandeBundle/Controller/ApiController.php
@@ -8,9 +8,12 @@ use FOS\RestBundle\FOSRestBundle;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as Con;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends FOSRestController
 {
@@ -21,6 +24,26 @@ class ApiController extends FOSRestController
     public function getDemandeAction(Request $req,$id){
         return $this->getDoctrine()->getRepository('DemandeBundle:Demande')->find($id);
     }
+
+
+
+    /**
+     * @Route("/getList", name="getList")
+     * @Method({"Get"})
+     * @View(serializerGroups={"default", "demande"})
+     */
+    public function getListAction()
+    {
+        $articles = $this->getDoctrine()->getRepository('DemandeBundle:Demande')->findAll();
+        $data = $this->get('jms_serializer')->serialize($articles, 'json');
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+
 
 
 

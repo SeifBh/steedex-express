98375b0a3ba3156d6e780c54c8756afd36647ade
SEIF BH HEROKU <belhadjali.seif@gmail.com>
2019-08-20 18:21:39 +0100

    before validation

--- tests/ReclamationBundle/Controller/DefaultControllerTest.php
+++ tests/ReclamationBundle/Controller/DefaultControllerTest.php
@@ -2,16 +2,16 @@
 
 namespace ReclamationBundle\Tests\Controller;
 
+use ReclamationBundle\Entity\Reclamation;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $reclamation = new Reclamation();
+        $this->assertSame(0, $reclamation->getId());
     }
 }

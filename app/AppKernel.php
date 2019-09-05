233083900904e7a13127bf0eaee5cea5a0ca0dd6
SEIF BH HEROKU <belhadjali.seif@gmail.com>
2019-09-05 16:12:51 +0100

    steedex final v1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,7 +36,7 @@ class AppKernel extends Kernel
 
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev', 'test'], false)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
